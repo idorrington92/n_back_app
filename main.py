0d97c74a3a00c0636aa9e021a9257e8b188b93e0
iain <idorrington92@gmail.com>
2020-10-03 16:30:16 +0100

    First attempt at menu navigation

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.bottomnavigation import MDBottomNavigation
+from kivymd.uix.picker import MDThemePicker
+
+
 
 class MainApp(MDApp):
-    def open_about(self, *args):
-        print("Opening about page")
+    def open_page(self, page):
+        print(f"Opening {page} page")
 
+class Level1Window(Screen):
+    pass
 
+class Level2Window(Screen):
+    pass
+
+class Level3Window(Screen):
+    pass
+
+class HighScoreWindow(Screen):
+    pass
+
+class SettingsWindow(Screen):
+    pass
+
+class AboutWindow(Screen):
+    pass
+
+class MainWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class NavigationBar(MDBottomNavigation):
+    pass
 MainApp().run()
