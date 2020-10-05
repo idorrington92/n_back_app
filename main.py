71709e9a0600d7be0842267e80aca0c49f7ac06b
iain <idorrington92@gmail.com>
2020-10-05 20:43:42 +0100

    Moved menu out of this repo for now, to allow focus on the game screen. Can generate a grid of squares and a random square light up every three seconds

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivymd.uix.bottomnavigation import MDBottomNavigation
-from kivymd.uix.picker import MDThemePicker
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.graphics import Rectangle, Color
+from random import randint
 
 
+class Grid(Widget):
+    def __init__(self):
+        super().__init__()
+        self.sqsize = 200
+        self.lightstep = 1
+
+    def update_grid(self, *args):
+        with self.canvas:
+            # Add a rectangle
+            grid = {}
+            highlight = ()
+            if self.lightstep%3==0:
+                highlight = (randint(0, 2), randint(0, 2))
+            self.lightstep += 1
+            for row in range(3):
+                grid[row] = {}
+                for col in range(3):
+                    Color(0, 0, 0.5)
+                    if highlight == (row, col):
+                        Color(0, 0, 1)
+                    grid[row][col] = Rectangle(pos=(row*self.sqsize*1.1+10, col*1.1*self.sqsize + 10),
+                                               size=(self.sqsize, self.sqsize))
 
 class MainApp(MDApp):
-    def open_page(self, page):
-        print(f"Opening {page} page")
+    def build(self):
+        grid = Grid()
+        Clock.schedule_interval(grid.update_grid, 1)
+        return grid
 
-class Level1Window(Screen):
-    pass
 
-class Level2Window(Screen):
-    pass
 
-class Level3Window(Screen):
-    pass
-
-class HighScoreWindow(Screen):
-    pass
-
-class SettingsWindow(Screen):
-    pass
-
-class AboutWindow(Screen):
-    pass
-
-class MainWindow(Screen):
-    pass
-
-class WindowManager(ScreenManager):
-    pass
-
-class NavigationBar(MDBottomNavigation):
-    pass
-MainApp().run()
+app = MainApp()
+app.run()
