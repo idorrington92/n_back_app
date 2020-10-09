45685608a5c9060b23eae08083d7c98034609bca
iain <idorrington92@gmail.com>
2020-10-09 23:03:24 +0100

    Remade the kv file to make each tile of the grid a widget. This allows ids for each tile (can't id widgets within a canvas). Can't figure out how to get the tiles to light up with the clock now, but can change the 'state' property of the tiles as a first step.

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
-from kivy.graphics import Rectangle, Color
-from random import randint
-from kivy.graphics.instructions import Canvas
+from numpy.random import randint
 
-class NBackGrid(Widget):
-    pass
+
+class NBackTile(Widget):
+    def __init__(self, **kwargs):
+        super(NBackTile, self).__init__(**kwargs)
+        self.state = "dark"
+
+
+    def printIDs(self):
+        print(self.ids)
+        self.state = "lit"
 
 class NBackGame(Widget):
     def __init__(self):
         super().__init__()
-        self.sqsize = 200
         self.lightstep = 1
+        self.tiles = ['upperl', 'upperm', 'upperr',
+                      'centerl', 'centertile', 'centerr',
+                      'lowerl', 'lowerm', 'lowerr']
+
+    def printIDs(self):
+        print(self.ids)
 
     def update_grid(self, *args):
+        self.canvas.clear()
         with self.canvas:
+            self.sqsize = self.size[1] / 4
             # Add a rectangle
-            grid = {}
-            highlight = ()
-            if self.lightstep%3==0:
-                highlight = (randint(0, 2), randint(0, 2))
-            self.lightstep += 1
-            for row in range(3):
-                grid[row] = {}
-                for col in range(3):
-                    Color(0, 0, 0.5)
-                    if highlight == (row, col):
-                        Color(0, 0, 1)
-                    grid[row][col] = Rectangle(pos=(row*self.sqsize*1.1+10, col*1.1*self.sqsize + 10),
-                                               size=(self.sqsize, self.sqsize))
+            if self.lightstep%3==1:
+                highlight = self.tiles[randint(9)]
+                self.ids[highlight].state = 'lit'
+                print(highlight)
+                self.lightstep += 1
+
 
 class NBackApp(MDApp):
     def build(self):
-        #grid = NBackGame()
-        #Clock.schedule_interval(grid.update_grid, 1)
-        return NBackGame() #grid
+        grid = NBackGame()
+        Clock.schedule_interval(grid.update_grid, 1)
+        return grid
+
+    def printIDs(self):
+        print(self.root.ids)
 
 NBackApp().run()
