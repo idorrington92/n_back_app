b060b5fd12377b126d2b50da0ff8df76dbd55f53
iain <idorrington92@gmail.com>
2020-10-10 15:05:10 +0100

    The tiles now light up and change colour using the theme colours.

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from numpy.random import randint
-
+from kivy.animation import Animation
 
 class NBackTile(Widget):
     def __init__(self, **kwargs):
         super(NBackTile, self).__init__(**kwargs)
-        self.state = "dark"
 
+    def highlight_tile(self):
+        anim = Animation(animated_color=MDApp.get_running_app().theme_cls.primary_light, duration=.5)
+        anim.bind(on_complete=self.reset)
+        anim.start(self)
+
+    def reset(self, *args):
+        anim = Animation(animated_color=MDApp.get_running_app().theme_cls.primary_dark, duration=.5)
+        #self.animated_color = MDApp.get_running_app().theme_cls.primary_dark
+        anim.start(self)
 
     def printIDs(self):
         print(self.ids)
-        self.state = "lit"
+        print(self.state)
 
 class NBackGame(Widget):
     def __init__(self):
@@ -26,24 +34,24 @@ class NBackGame(Widget):
         print(self.ids)
 
     def update_grid(self, *args):
-        self.canvas.clear()
-        with self.canvas:
-            self.sqsize = self.size[1] / 4
-            # Add a rectangle
-            if self.lightstep%3==1:
-                highlight = self.tiles[randint(9)]
-                self.ids[highlight].state = 'lit'
-                print(highlight)
-                self.lightstep += 1
+        # Add a rectangle
+        if self.lightstep%3==1:
+            self.highlight = self.tiles[randint(9)]
+            self.ids[self.highlight].highlight_tile()
+
+        self.lightstep += 1
 
 
 class NBackApp(MDApp):
     def build(self):
-        grid = NBackGame()
-        Clock.schedule_interval(grid.update_grid, 1)
-        return grid
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Indigo"
+        game = NBackGame()
+        Clock.schedule_interval(game.update_grid, 1.)
+        return game
 
     def printIDs(self):
         print(self.root.ids)
+
 
 NBackApp().run()
