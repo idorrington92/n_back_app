3d5ae5df9a1d817426fbce2a5da4faf108310071
iain <idorrington92@gmail.com>
2020-10-17 18:55:19 +0100

    Proper scoring system now. Changed the random generation so that there is a guarenteed 10 repeats at least. Player will lose 100 points if they are wrong.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from numpy.random import randint
+from random import sample
 from kivy.animation import Animation
 from kivy.properties import NumericProperty, ObjectProperty, ListProperty
 
@@ -21,7 +22,6 @@ class NBackTile(Widget):
 
     def reset(self, *args):
         anim = Animation(animated_color=MDApp.get_running_app().theme_cls.primary_dark, opacity=.05, duration=.5)
-        #self.animated_color = MDApp.get_running_app().theme_cls.primary_dark
         anim.start(self)
 
     def printIDs(self):
@@ -32,10 +32,29 @@ class GridLine(Widget):
     pass
 
 class NBackGame(Widget):
+    # Initialise score
     score = NumericProperty(0)
-    #super(NBackGame, self).__init__()
-    lightstep = NumericProperty(1)
-    tiles = ListProperty(['upperl', 'upperm', 'upperr', 'centerl', 'centertile', 'centerr', 'lowerl', 'lowerm', 'lowerr'])
+    # N back
+    N = 2
+    # Time step in game. Tile lights up when a multiple of 3.
+    timestep = NumericProperty(0)
+    # Counts how many times tiles have lit up.
+    lightstep = NumericProperty(0)
+    # Length of round. e.g. 40 iterations at 3 seconds makes a 2 minute round.
+    num_iter = N * 12
+    # Choose 10 indexes to be guarenteed repeats
+    rand_doubles = sample(range(N, num_iter-10), 10)
+    # Create the random order for the tiles
+    random_order = []
+    for i in range(num_iter):
+        if i in rand_doubles:
+            random_order.append(random_order[-N])
+        else:
+            random_order.append(randint(9))
+
+    tiles = ListProperty(['upperl', 'upperm', 'upperr', 'centerl',
+                          'centertile', 'centerr', 'lowerl', 'lowerm',
+                          'lowerr'])
 
     def printIDs(self):
         print(self.ids)
@@ -44,18 +63,28 @@ class NBackGame(Widget):
         # Was button press correct?
         # Update score in app
         self.update_score(100)
-        print(self.score)
 
     def update_grid(self, *args):
         # Add a rectangle
-        if self.lightstep%3==1:
-            self.highlight = self.tiles[randint(9)]
+        if self.lightstep > self.num_iter:
+            # End game
+            pass
+
+        if self.timestep%3==1:
+            self.lightstep += 1
+            self.highlight = self.tiles[self.random_order[self.lightstep]]
             self.ids[self.highlight].highlight_tile()
 
-        self.lightstep += 1
+
+        self.timestep += 1
 
     def update_score(self, dscore):
-        self.score += dscore
+        # Check if player was correct/incorrect and increase/decrease their score accordingly.
+        if self.random_order[self.lightstep] == self.random_order[self.lightstep - self.N] and self.N < self.lightstep:
+            self.score += dscore
+        else:
+            self.score -= dscore
+
 
 
 
